b4b12de381fa00f5f07866607735bcd792f04adb
FGVel0ciTy <minecraftsurvivor231@gmail.com>
2017-10-01 20:35:07 -0700

    Added Camera Script

---
+++ SkillsUSA2017-18/Assets/Scripts/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset;
+    public float m_DampTime = 0.2f;                 // Approximate time for the camera to refocus.
+    public float m_ScreenEdgeBuffer = 4f;           // Space between the top/bottom most target and the screen edge.
+    private float m_ZoomSpeed;                      // Reference speed for the smooth damping of the orthographic size.
+    private Vector3 m_MoveVelocity;                 // Reference velocity for the smooth damping of the position.
+    private Vector3 m_DesiredPosition;              // The position the camera is moving towards.
+
+    // Use this for initialization
+    void Start ()
+    {
+        offset = transform.position - player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + offset, ref m_MoveVelocity, m_DampTime);
+    }
+}
