064d2f5a2b8469c9d2864abc31a619477cd49646
Judah <chickenscratchcoder@gmail.com>
2017-10-03 00:26:39 -0700

    Added Pings
    Right click to shoot out pings. (1 per second)
    Pings bounce 5 times, creating shrinking lights at each bounce. Each bounce makes an increasingly dim light.
    TODO: indicate when a ping is shot, due to rate of fire.
    issues occur when too many point lights are in the scene

---
+++ SkillsUSA2017-18/Assets/Scripts/LightFade.cs
@@ -0,0 +1,34 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFade : MonoBehaviour {
+    Light lt;
+    public float timeToFade = 1f;
+    float initIntens;
+    float elapsedTime;
+
+	// Use this for initialization
+	void Start () {
+        lt = GetComponent<Light>();
+        elapsedTime = 0;
+        initIntens = lt.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (elapsedTime >= timeToFade)
+        {
+            Destroy(this.gameObject);
+        }
+        lt.intensity = Mathf.Lerp(initIntens, 0, elapsedTime / timeToFade);
+        elapsedTime += Time.deltaTime;
+	}
+
+    public void SetIntensity (float intens)
+    {
+        GetComponent<Light>().intensity = intens;
+        initIntens = intens;
+        print(intens);
+    }
+}
---
+++ SkillsUSA2017-18/Assets/Scripts/Ping.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ping : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public GameObject player;
+    bool wasPinging;
+    public GameObject PingLight;
+    public float lightZOffset = -3f;
+    public float firstLightIntensity = 6f;
+
+    // num collisions with wall
+    int collCount;
+
+    // collisions until deactivation
+    public int totalColls = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+        collCount = 0;
+        FirePing();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collCount >= totalColls)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    // todo: make new instance
+    void FirePing()
+    {
+        transform.position = player.transform.position;
+        Vector2 mousePos = new Vector2(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2);
+        rb.AddForce(mousePos.normalized * 500);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        collCount++;
+        // spawn new light
+
+
+        // TODO: FIX FOR BARREL OFFSET VALUE: (0.211, 0.055)
+        Vector3 pos = transform.position;
+        pos.z = lightZOffset;
+
+        PingLight.SetActive(true);
+        GameObject light = Instantiate(PingLight, pos, transform.rotation);
+        PingLight.SetActive(false);
+
+        light.SendMessage("SetIntensity", firstLightIntensity / collCount);
+    }
+}
