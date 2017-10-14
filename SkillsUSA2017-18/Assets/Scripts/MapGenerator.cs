4289c9908175de61b5f4342c6cfa0966bb39a0cf
FGVel0ciTy <minecraftsurvivor231@gmail.com>
2017-10-14 14:14:59 -0700

    Added Map Generation Script

---
+++ SkillsUSA2017-18/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+
+    int[,] map;
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        map = new int[width, height];
+    }
+}
