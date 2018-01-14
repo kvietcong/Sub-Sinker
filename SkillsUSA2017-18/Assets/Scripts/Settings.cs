9a522e50adbfda7bac1599eaba2cec007d412ad1
FGVel0ciTy <minecraftsurvivor231@gmail.com>
2018-01-13 19:05:38 -0800

    Volume and Quality settings

---
+++ SkillsUSA2017-18/Assets/Scripts/Settings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public Slider musicLevel;
+    public Slider sensitivityLevel;
+
+    void Update()
+    {
+        AudioListener.volume = musicLevel.value;
+
+    }
+
+    public void onClick()
+    {
+        GetComponent<Animator>().SetBool("TrigOn", true);
+        GetComponent<Animator>().SetBool("TrigOff", false);
+    }
+
+    public void onBack()
+    {
+        GetComponent<Animator>().SetBool("TrigOff", true);
+        GetComponent<Animator>().SetBool("TrigOn", false);
+    }
+
+    public void setQuality(int i)
+    {
+        QualitySettings.SetQualityLevel(i, true);
+    }
+}
