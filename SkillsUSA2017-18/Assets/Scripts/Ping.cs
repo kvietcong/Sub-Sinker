757b605a491b0db6701c9d9150d5f7b4e2945741
imfrendy <chickenscratchcoder@gmail.com>
2018-01-13 16:41:54 -0800

    Revert "Merge branch 'master' into Beta"
    This reverts commit 8656602327a9dff228a2a4289d59e089ddd05097, reversing
    changes made to 6421caa750b99d21c48861ec1add0563348a3dd8.

---
+++ SkillsUSA2017-18/Assets/Scripts/Ping.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ping : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public GameObject player;
+    bool wasPinging;
+    public GameObject PingLight;
+    public float lightZOffset = -3f;
+    public float firstLightIntensity = 6f;
+    public float force;
+
+    // num collisions with wall
+    int collCount;
+
+    // collisions until deactivation
+    public int totalColls = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+        collCount = 0;
+        FirePing();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collCount >= totalColls)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    // todo: make new instance
+    void FirePing()
+    {
+        transform.position = player.transform.position;
+        Vector2 mousePos = new Vector2(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2);
+        rb.AddForce(mousePos.normalized * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        collCount++;
+        // spawn new light
+
+
+        Vector3 pos = transform.position;
+        pos.z = lightZOffset;
+
+        PingLight.SetActive(true);
+        GameObject light = Instantiate(PingLight, pos, transform.rotation);
+        PingLight.SetActive(false);
+
+        light.SendMessage("SetIntensity", firstLightIntensity / collCount);
+    }
+}
---
+++ SkillsUSA2017-18/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public float illuminationRadius=10;
+    public Light light;
+    public float maxRad=25;
+    public float minRad=2;
+
+    private Rigidbody2D rb;
+    private float currentRad;
+    private float scrollSpeed;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentRad = illuminationRadius;
+        light.range = currentRad;
+        scrollSpeed = 10;
+    }
+
+    private void Update()
+    {
+        if(currentRad<minRad)
+        {
+            currentRad = minRad;
+        }
+        if (currentRad > maxRad)
+        {
+            currentRad = maxRad;
+        }
+        light.range = currentRad;
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 1 && currentRad >= minRad)
+        {
+            currentRad -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 1 && currentRad <= maxRad)
+        {
+            currentRad += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+        rb.AddForce (movement.normalized * speed);
+    }
+}
