b1619a421ca81430e20acba2463ab14680a1b023
imfrendy <chickenscratchcoder@gmail.com>
2018-01-27 16:47:32 -0800

    basic functionality for data persistence and profile system

---
+++ SkillsUSA2017-18/Assets/Scripts/GlobalData.cs
@@ -0,0 +1,25 @@
+﻿public static class GlobalData {
+    private static bool scrollInverse;
+
+    // example
+    //private static int kills, deaths, assists, points;
+
+    public static bool ScrollInverse
+    {
+        get { return scrollInverse;  }
+        set { scrollInverse = value; }
+    }
+
+    //// example
+    //public static int Points
+    //{
+    //    get
+    //    {
+    //        return points;
+    //    }
+    //    set
+    //    {
+    //        points = value;
+    //    }
+    //}
+}
