ae797103877d959c62ac931607435d355bc0f015
FGVel0ciTy <minecraftsurvivor231@gmail.com>
2018-02-19 16:02:01 -0800

    ColorChanger
    :(

---
+++ SkillsUSA2017-18/Assets/Scripts/UI/ColorPickerUpdate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPickerUpdate : MonoBehaviour
+{
+
+    public Image primary;
+    public Image secondary;
+    public Image decor;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        primary.color = GameManager.instance.playerSettings.PlayerPrimaryColor;
+        secondary.color = GameManager.instance.playerSettings.PlayerSecondaryColor;
+        decor.color = GameManager.instance.playerSettings.PlayerDecorColor;
+    }
+}
---
+++ SkillsUSA2017-18/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public Text playerName;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        GameManager.instance.playerSettings.PlayerName = playerName.text;
+	}
+}
