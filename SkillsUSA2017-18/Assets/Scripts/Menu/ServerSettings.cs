d4836501c7d15925c3dac8df49c290631a29b8b2
FGVel0ciTy <minecraftsurvivor231@gmail.com>
2018-04-18 18:52:16 -0700

    Stuff

---
+++ SkillsUSA2017-18/Assets/Scripts/Menu/ServerSettings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ServerSettings : MonoBehaviour
+{
+    public static ServerManager instance;
+    public int mapHeight = 80;
+    public int mapWidth = 80;
+    public int respawnTime = 3;
+
+    public Slider height;
+    public Slider width;
+    public Slider respawn;
+    public Text widthIndicator;
+    public Text heightIndicator;
+    public Text respawnIndicator;
+
+    // Use this for initialization
+    void Start ()
+    {
+        height.value = mapHeight;
+        width.value = mapWidth;
+        respawn.value = respawnTime;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        widthIndicator.text = width.value.ToString();
+        heightIndicator.text = height.value.ToString();
+        respawnIndicator.text = respawn.value.ToString();
+        
+        ServerManager.instance.mapWidth = (int)width.value;
+        ServerManager.instance.mapHeight = (int)height.value;
+        ServerManager.instance.respawnTime = (int)respawn.value;
+    }
+}
