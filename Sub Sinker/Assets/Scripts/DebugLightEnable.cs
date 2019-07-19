c091d99ca02a62aee63281c932db1687b67795bf
Judah Wyllie <judahw@uw.edu>
2019-07-18 19:32:12 -0700

    Revert "Revert "Merge pull request #1 from imfrendy/Issue-15-Unity-Update""
    This reverts commit 840dd3b1ad11a2e5e36dd6cf1a8c1d0162fb42b0.

---
+++ Sub Sinker/Assets/Scripts/DebugLightEnable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugLightEnable : MonoBehaviour {
+    Light debugLight;
+
+    void Start()
+    {
+        debugLight = GetComponent<Light>();
+        debugLight.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Debug.isDebugBuild)
+        {
+            // debug light toggle
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                debugLight.enabled = !debugLight.enabled;
+            }
+        }
+    }
+}
---
+++ Sub Sinker/Assets/Scripts/Fish.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour {
+    Rigidbody2D rb;
+    public float forceScale;
+    float baseSpeed;
+    int dir; // -1 = left
+	// Use this for initialization
+	void Start () {
+        Color jellyColor = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f, 0.75f, 0.75f);
+        GetComponent<MeshRenderer>().materials[0].color = jellyColor;
+        gameObject.transform.localScale = new Vector3(Random.Range(1f, 3f), Random.Range(1f, 3f), Random.Range(1f, 3f));
+
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+        dir = Random.Range(0, 1) * 2 - 1;
+        baseSpeed = Random.Range(0, 15f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(new Vector2(Random.Range(baseSpeed - 2f, baseSpeed + 2f) * Time.deltaTime * dir, 0) * forceScale);
+
+        if (dir == -1)
+        {
+            transform.eulerAngles = new Vector3();
+        }
+        else if (dir == 1)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        dir *= -1;
+    }
+}
